b507923a2e463ba7828e3db6337debb34a8633c3
João Cardoso <jg235711p@gmail.com>
2026-03-23 19:15:40 +0000

    Aula0323 entrada e calculo de dados

---
+++ fundamentos_de_dart/bin/media.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main(){
+  stdout.write("Escreva a nota da prova:");
+  String? valor1 = stdin.readLineSync();
+  double? prova = double.tryParse(valor1!) ?? 0.0;
+  
+  stdout.write("Escreva a nota do trabalho:");
+  double trabalho = double.tryParse(stdin.readLineSync()!) ?? 0.0;
+  
+  double? media = prova * 0.6 + trabalho * 0.4;
+
+  stdout.write('Sua média é: $media');
+}
